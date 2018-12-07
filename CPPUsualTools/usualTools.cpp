7b87b23c9a6ad8c99b3ae35d6dca133e5181ba26
LancerMar <602545140@qq.com>
2018-12-07 15:38:20 +0800

    add IP Transfer

--- CPPUsualTools/usualTools.cpp
+++ CPPUsualTools/usualTools.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "tools.h"
+#include <string>
+#include <cstring>
 
 char * TransHLAndByte(unsigned char * testByte,int byteLen)
 {
@@ -21,4 +23,32 @@ char * TransHLAndByte(unsigned char * testByte,int byteLen)
 	}
 
 	return result;
+}
+
+//16进制ip转化为10进制字符型IP
+void ipTstrIP(unsigned int ip, char *ipchar)
+{
+	char ip_temp[64];
+	std::string str = "";
+	int temp;
+	int ipSize = sizeof(ip);
+
+	for (int i = 0; i < ipSize; i++)
+	{
+		temp = (ip >> 8 * (ipSize - 1 - i)) & 0xFF;
+		str += std::to_string(temp) + ".";
+	}
+	str.erase(str.end() - 1);
+	//cout << str << endl;
+
+	//cout << str.length() << endl;
+
+	int i = 0;
+	for (i = 0; i < str.length(); i++)
+	{
+		*(ipchar + i) = str[i];
+	}
+	*(ipchar + i) = '\0';
+	//strcpy(ipchar, ip_temp);
+
 }
